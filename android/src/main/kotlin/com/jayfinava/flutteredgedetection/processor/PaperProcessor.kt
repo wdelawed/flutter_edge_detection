05d3174f7da54d9e7afc585103069edb8ea246ea
Mohamed Alawad <m.elawed@walaplus.com>
2026-02-02 18:15:41 +0300

    update parameters

--- android/src/main/kotlin/com/jayfinava/flutteredgedetection/processor/PaperProcessor.kt
+++ android/src/main/kotlin/com/jayfinava/flutteredgedetection/processor/PaperProcessor.kt
@@ -21,8 +21,8 @@ private const val BORDER_MARGIN_PX = 12.0
 private const val PREVIEW_KEEP_LAST_MS = 500L
 private const val PREVIEW_MAX_AVG_MOVE_PX = 10.0
 private const val PREVIEW_MAX_CENTER_MOVE_PX = 10.0
-private const val PREVIEW_MIN_AREA_SCALE = 0.75
-private const val PREVIEW_MAX_AREA_SCALE = 1.25
+private const val PREVIEW_MIN_AREA_SCALE = 0.90
+private const val PREVIEW_MAX_AREA_SCALE = 1.10
 private const val PREVIEW_SMOOTH_ALPHA = 0.24
 
 // ----- Public API -----
